f9ec1ee22c939fbf0742694a3b8974617167337f
Joao Guilherme <pike.joaoberchof@gmail.com>
2021-05-07 16:26:03 -0300

    IF ELSE

---
+++ aula11/exerc04.js
@@ -0,0 +1,6 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log (`Agora são exatamente ${hora}hrs.`)
+if (hora < 12 ) {console.log  ('Bom Dia !!')}
+else if (hora <= 18 ) {console.log ('Boa Tarde!!')}
+else { console.log  ('Boa noite !!')}
--- aula11/exerc3.js
+++ aula11/exerc3.js
@@ -1,6 +1,7 @@
-var idade = "22"
+var idade = "15"
+console.log (`Você tem ${idade} anos.`)
 if (idade < 16) {
     console.log('Não vota.')
-} else {
- if (idade <= 16 && idade < 18) {console.log ('Voto opcional')}
+} else if (idade < 18 || idade > 65){
+   console.log ('Voto opcional')
 } 
