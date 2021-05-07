342c19096fe827c5b5b3d4124c928731dbf348ed
Joao Guilherme <pike.joaoberchof@gmail.com>
2021-05-07 15:41:13 -0300

    Estudos JavaScript

--- aula11/exerc3.js
+++ aula11/exerc3.js
@@ -1,6 +1,6 @@
 var idade = "22"
-if (idade < 18) {
-    console.log('Menor de idade.')
+if (idade < 16) {
+    console.log('Não vota.')
 } else {
-console.log ('Maior de idade')
+ if (idade <= 16 && idade < 18) {console.log ('Voto opcional')}
 } 
